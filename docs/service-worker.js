be4f3e97ace6b905fd43b25867160c87524cf695
420swap <80228887+420swap@users.noreply.github.com>
2021-03-17 02:20:02 -0300

    1

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c168872474f4baf4a3d56c3922af649e.js"
+  "/precache-manifest.de4c142f3d05d175b1f212dc4d4817a3.js"
 );
 
 self.addEventListener('message', (event) => {
